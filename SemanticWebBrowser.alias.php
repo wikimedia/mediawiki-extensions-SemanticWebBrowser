15cb2ceb87131d7e719baec86ef1cc46445a2862
Siebrand Mazeland <siebrand@users.mediawiki.org>
2012-05-23 19:28:24 +0000

    Localisation updates from http://translatewiki.net.

--- SemanticWebBrowser.alias.php
+++ SemanticWebBrowser.alias.php
@@ -16,10 +16,30 @@ $specialPageAliases['en'] = array(
 
 /** German (Deutsch) */
 $specialPageAliases['de'] = array(
-	'BrowseWiki' => array( 'Wiki_browsen' ),
+	'BrowseWiki' => array( 'Wiki_durchsuchen', 'Wiki_browsen' ),
+);
+
+/** Swiss German (Alemannisch) */
+$specialPageAliases['gsw'] = array(
+	'BrowseWiki' => array( 'Wiki_duresueche' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'BrowseWiki' => array( 'ПрелистајВики' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'BrowseWiki' => array( 'വിക്കിബ്രൗസ്ചെയ്യുക' ),
 );
 
 /** Dutch (Nederlands) */
 $specialPageAliases['nl'] = array(
 	'BrowseWiki' => array( 'WikiDoorbladeren' ),
+);
+
+/** Simplified Chinese (‪中文(简体)‬) */
+$specialPageAliases['zh-hans'] = array(
+	'BrowseWiki' => array( '浏览维基' ),
 );
